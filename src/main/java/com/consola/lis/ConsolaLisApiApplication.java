5b0e0b5d4e34f4aa769c3726ac0ccaa9bfc8317f
superpollo2 <laura.tascon@udea.edu.co>
2024-03-17 14:50:32 -0500

    Initial construction of the project using MySQL, 	JPA, Lombok using n-tier architecture.

---
+++ src/main/java/com/consola/lis/ConsolaLisApiApplication.java
@@ -0,0 +1,13 @@
+package com.consola.lis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsolaLisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsolaLisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consola/lis/ConsolaLisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consola.lis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsolaLisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
