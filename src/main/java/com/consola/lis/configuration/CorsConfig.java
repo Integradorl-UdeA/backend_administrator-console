27634cab8d542a682a26a580c4e279910195a323
superpollo2 <laura.tascon@udea.edu.co>
2024-03-28 21:09:59 -0500

    Improve Dockerfile and include CORS configuration
    This commit updates the Dockerfile to enhance the deployment process. Additionally, it adds CORS configuration to enable connections to the API from a React server. This CORS setup ensures smooth communication between the backend and the frontend, allowing seamless interaction between the two components.

---
+++ src/main/java/com/consola/lis/configuration/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.consola.lis.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/console-lis/**").allowedOrigins("http://localhost:3000").allowedHeaders("*").allowedMethods("*");
+            }
+        };
+    }
+}
