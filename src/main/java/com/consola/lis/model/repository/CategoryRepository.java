0fca043361a7db5bd60323b524d634faf8b90357
superpollo2 <laura.tascon@udea.edu.co>
2024-03-30 03:23:05 -0500

    Remove unused import

--- src/main/java/com/consola/lis/model/repository/CategoryRepository.java
+++ src/main/java/com/consola/lis/model/repository/CategoryRepository.java
@@ -1,7 +1,6 @@
 package com.consola.lis.model.repository;
 
 import com.consola.lis.model.entity.Category;
-import com.consola.lis.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
