705dcfb2db619985352e8699b037ec9a07f08447
superpollo2 <laura.tascon@udea.edu.co>
2024-04-03 06:44:10 -0500

    add enpoints for user

--- src/main/java/com/consola/lis/controller/UserController.java
+++ src/main/java/com/consola/lis/controller/UserController.java
@@ -2,11 +2,14 @@ package com.consola.lis.controller;
 
 
 import com.consola.lis.constans.EndpointConstant;
+import com.consola.lis.dto.AuthResponseDTO;
 import com.consola.lis.dto.UserDTO;
 import com.consola.lis.model.entity.User;
 import com.consola.lis.model.enums.UserRole;
 import com.consola.lis.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -21,14 +24,15 @@ public class UserController {
         return userService.getUser(userId);
     }
 
-    @PatchMapping("/{username}/role")
-    public User changeRole(@PathVariable("username") String username, @RequestBody UserRole userRole){
-        return userService.changeUserRole(username, userRole);
+
+    @PatchMapping("/role/{username}")
+    public ResponseEntity<AuthResponseDTO> changeRole(@PathVariable("username") String username, @RequestBody UserRole userRole){
+        return ResponseEntity.ok(userService.changeUserRole(username, userRole));
     }
 
     @GetMapping("/ldapUser/{username}")
-    public UserDTO userLDAP(@PathVariable("username") String username){
-        return userService.getUserLDAP(username);
+    public ResponseEntity<UserDTO> userLDAP(@PathVariable("username") String username){
+        return ResponseEntity.ok(userService.getUserLDAP(username));
     }
 
     @DeleteMapping("/{username}")
--- src/main/java/com/consola/lis/service/UserService.java
+++ src/main/java/com/consola/lis/service/UserService.java
@@ -1,7 +1,9 @@
 package com.consola.lis.service;
 
+import com.consola.lis.dto.AuthResponseDTO;
 import com.consola.lis.dto.UserDTO;
 import com.consola.lis.exception.NotExistingException;
+import com.consola.lis.jwt.JwtService;
 import com.consola.lis.model.entity.User;
 import com.consola.lis.model.enums.UserRole;
 import com.consola.lis.model.repository.UserRepository;
@@ -21,6 +23,7 @@ public class UserService {
     public final UserRepository userRepository;
     private final RestTemplate restTemplate;
     public final PasswordEncoder passwordEncoder;
+    public final JwtService jwtService;
 
     public User getUser(String username) {
         Optional<User> user = userRepository.findByUsername(username);
@@ -31,17 +34,9 @@ public class UserService {
         }
     }
 
-    public void changePassword(String username, String newPassword) {
-        Optional<User> user = userRepository.findByUsername(username);
-        if (user.isEmpty()) {
-            throw new NotExistingException("404", HttpStatus.NOT_FOUND, " the user whit username " + username + " not exist");
-        }
-        user.get().setPassword(passwordEncoder.encode(newPassword));
-        userRepository.save(user.get());
 
-    }
 
-    public User changeUserRole(String username, UserRole newRole) {
+    public AuthResponseDTO changeUserRole(String username, UserRole newRole) {
 
         Optional<User> user = userRepository.findByUsername(username);
         if (user.isEmpty()) {
@@ -49,7 +44,12 @@ public class UserService {
         }
 
         user.get().setRole(newRole);
-        return userRepository.save(user.get());
+        userRepository.save(user.get());
+
+
+        return AuthResponseDTO.builder()
+                .token(jwtService.getToken(user.get()))
+                .build();
     }
 
 
