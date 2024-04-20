5ede5a34d7ffad31919aac3b68dd6398e92de7d0
superpollo2 <laura.tascon@udea.edu.co>
2024-04-19 19:22:14 -0500

    exclude JSONObjet of Android

--- src/main/java/com/consola/lis/util/mapper/InventoryItemMapper.java
+++ src/main/java/com/consola/lis/util/mapper/InventoryItemMapper.java
@@ -3,6 +3,7 @@ package com.consola.lis.util.mapper;
 import com.consola.lis.dto.ItemInfoDTO;
 import com.consola.lis.model.entity.Category;
 import com.consola.lis.model.entity.GeneralItem;
+import com.consola.lis.model.entity.QuantizableItem;
 import org.json.JSONObject;
 
 import java.util.Set;
@@ -10,7 +11,7 @@ import java.util.Set;
 public class InventoryItemMapper {
     private InventoryItemMapper () {
     }
-    public static ItemInfoDTO mapToItemInfoDTO(GeneralItem generalItem, Category category) {
+    public static ItemInfoDTO mapToItemInfoGene(GeneralItem generalItem, Category category) {
 
         return ItemInfoDTO.builder()
                 .id(generalItem.getItemId())
@@ -21,6 +22,20 @@ public class InventoryItemMapper {
                 .attributes(convertAttributes(generalItem.getAttributes()))
                 .build();
     }
+
+    public static ItemInfoDTO mapToItemInfoQuant(QuantizableItem quantizableItem, Category category) {
+
+        GeneralItem generalItem = quantizableItem.getGeneralItem();
+        return ItemInfoDTO.builder()
+                .id(String.valueOf(quantizableItem.getQuantizableItemId()))
+                .state(String.valueOf(generalItem.getState()))
+                .category(category.getCategoryName())
+                .wallet(String.valueOf(generalItem.getWallet()).replace("_"," "))
+                .quantizable(category.getQuantizable())
+                .attributes(convertAttributes(generalItem.getAttributes()))
+                .build();
+    }
+
 
 
     private static String[] convertAttributes(String attributes){
