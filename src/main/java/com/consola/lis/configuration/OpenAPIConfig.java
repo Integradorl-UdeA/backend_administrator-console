407a264f11542b3dda741f06a0e5012e9bb3c779
laura Tascón <laura.tascon@udea.edu.co>
2024-04-30 11:19:23 -0500

    enable configuration security for enpoints

--- src/main/java/com/consola/lis/configuration/OpenAPIConfig.java
+++ src/main/java/com/consola/lis/configuration/OpenAPIConfig.java
@@ -1,6 +1,7 @@
 package com.consola.lis.configuration;
 
 
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -24,10 +25,15 @@ public class OpenAPIConfig {
                 .scheme("bearer")
                 .bearerFormat("JWT");
 
+        //Define security requirement for endpoints that require Bearer token
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+               .addList("bearerAuth");
+
+        // Create OpenAPI object and configure servers, info, security schemes, and security requirements
         return new OpenAPI()
                 .info(info)
-                .components(new io.swagger.v3.oas.models.Components().addSecuritySchemes("bearerAuth", securityScheme));
-
+                .components(new io.swagger.v3.oas.models.Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .addSecurityItem(securityRequirement);
 
 
     }
