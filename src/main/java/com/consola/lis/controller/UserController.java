026088ca38d0f1cd0e64e85067b2b2b2d0cf5ab2
superpollo2 <laura.tascon@udea.edu.co>
2024-05-15 12:06:09 -0500

    Added Swagger annotations for controller and endpoint documentation
    Generated documentation for controllers and endpoints using Swagger
    annotations such as @Operation and @Tag. Descriptions are placed in
    a utility file to avoid cluttering the code.

--- src/main/java/com/consola/lis/controller/UserController.java
+++ src/main/java/com/consola/lis/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.consola.lis.controller;
 
 
+import com.consola.lis.util.constans.ApiDescription;
 import com.consola.lis.util.constans.EndpointConstant;
 import com.consola.lis.dto.AuthResponseDTO;
 import com.consola.lis.dto.UserDTO;
@@ -13,32 +14,36 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@Tag(name = "Manage users", description = "something")
+@Tag(name = "Manage users", description = ApiDescription.DESCRIPTION_CONTROLLER_USER)
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(EndpointConstant.ENDPOINT_USER)
 public class UserController {
 
     private final UserService userService;
+
+    @Operation(summary = ApiDescription.DESCRIPTION_ONE_USER)
     @GetMapping(EndpointConstant.ENDPOINT_ONE_USER)
     public ResponseEntity<UserDTO> oneUser(@PathVariable("username") String userId){
         return ResponseEntity.ok(userService.getUser(userId));
     }
 
 
-    @Operation(summary = "This method is used to login the options valid are ADMIN, AUXPROG, AUXADMI")
+    @Operation(summary = ApiDescription.DESCRIPTION_CHANGE_ROLE_USER)
     @PatchMapping(EndpointConstant.ENDPOINT_CHANGE_ROLE_USER)
     public ResponseEntity<AuthResponseDTO> changeRole(@PathVariable("username") String username, @RequestBody UserRole userRole){
         return ResponseEntity.ok(userService.changeUserRole(username, userRole));
     }
 
+    @Operation(summary = ApiDescription.DESCRIPTION_USER_LDAP)
     @GetMapping(EndpointConstant.ENDPOINT_USER_LDAP)
     public ResponseEntity<UserDTO> userLDAP(@PathVariable("username") String username){
         return ResponseEntity.ok(userService.getUserLDAP(username));
     }
 
+    @Operation(summary = ApiDescription.DESCRIPTION_DELETE_USER)
     @DeleteMapping(EndpointConstant.ENDPOINT_DELETE_USER)
     public void deleteUser(@PathVariable("username") String username){
-         userService.deleteUser(username);
+        userService.deleteUser(username);
     }
 }
