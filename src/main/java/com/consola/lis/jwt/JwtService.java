458c563100244011a9b83906fc7291867386c80c
David Upegui <100161899+DavidUpegui@users.noreply.github.com>
2024-05-19 23:26:42 -0500

    JWT token has now an expiration of 2 hours

--- src/main/java/com/consola/lis/jwt/JwtService.java
+++ src/main/java/com/consola/lis/jwt/JwtService.java
@@ -41,7 +41,7 @@ public class JwtService {
                 .claim("username", user.getUsername())
                 .subject(user.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 120))
                 .signWith(getKey())
                 .compact();
 
