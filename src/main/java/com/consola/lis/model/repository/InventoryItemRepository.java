3e367bbe58e503eb3829a1eb67b0f86da51d21c7
superpollo2 <laura.tascon@udea.edu.co>
2024-05-23 20:18:43 -0500

    fixed error in parse UserRole Enum

--- src/main/java/com/consola/lis/model/repository/InventoryItemRepository.java
+++ src/main/java/com/consola/lis/model/repository/InventoryItemRepository.java
@@ -2,6 +2,7 @@ package com.consola.lis.model.repository;
 
 import com.consola.lis.model.entity.InventoryItem;
 
+import com.consola.lis.model.enums.ItemState;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,4 +15,6 @@ public interface InventoryItemRepository extends JpaRepository<InventoryItem, St
 
     @Query("SELECT p FROM InventoryItem p ")
     Page<InventoryItem> findAllItems(Pageable pageable);
+
+    Page<InventoryItem> findByState(ItemState state, Pageable pageable);
 }
